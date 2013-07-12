249330b06b94627f3c5e3b20c634951c4fb7914f
Tsukasa Obara <obara@saucer.jp>
2013-07-12 10:06:00 +0900

    add Xcode source code

---
+++ clockSaver/clockSaverView.h
@@ -0,0 +1,16 @@
+//
+//  clockSaverView.h
+//  clockSaver
+//
+//  Created by Tsukasa Obara on 2013/07/12.
+//  Copyright (c) 2013 saucerjp. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+#import <WebKit/WebKit.h>
+
+@interface clockSaverView : ScreenSaverView
+{
+	WebView *webView;
+}
+@end
---
+++ clockSaver/clockSaverView.m
@@ -0,0 +1,35 @@
+//
+//  clockSaverView.m
+//  clockSaver
+//
+//  Created by Tsukasa Obara on 2013/07/12.
+//  Copyright (c) 2013 saucerjp. All rights reserved.
+//
+
+#import "clockSaverView.h"
+#import <WebKit/WebKit.h>
+
+@implementation clockSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+		webView = [[WebView alloc] initWithFrame:[self bounds] frameName:nil groupName:nil];
+        [webView setMainFrameURL:[NSString stringWithFormat:@"file://%@/src/index.html", [[NSBundle bundleForClass:[self class]] resourcePath]]];
+		[self addSubview:webView];
+    }
+    return self;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
